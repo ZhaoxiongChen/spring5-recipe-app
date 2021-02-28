00174cc93f716d03db634bbd979844c886fef05e
Zhaoxiong Chen <czx1997_czx@163.com>
2021-02-28 13:15:44 -0500

    8.137: Many To Many JPA Relationships

--- src/main/java/com/recipe/app/model/Recipe.java
+++ src/main/java/com/recipe/app/model/Recipe.java
@@ -17,8 +17,6 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
-    // todo add
-    // private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
@@ -26,19 +24,17 @@ public class Recipe {
     @Lob // LargeOBject type for images
     private Byte[] Image;
 
-    @OneToOne(cascade = CascadeType.ALL) // Recipe - Notes: 1 - 1
-    private Notes notes;
-
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
+    @OneToOne(cascade = CascadeType.ALL) // Recipe - Notes: 1 - 1
+    private Notes notes;
 
-    public void setDifficulty(Difficulty difficulty) {
-        this.difficulty = difficulty;
-    }
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+        joinColumns = @JoinColumn(name = "recipe_id"),
+ inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
 
     public Long getId() {
         return id;
@@ -104,6 +100,14 @@ public class Recipe {
         this.directions = directions;
     }
 
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
     public Byte[] getImage() {
         return Image;
     }
@@ -112,11 +116,27 @@ public class Recipe {
         Image = image;
     }
 
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
     public Notes getNotes() {
         return notes;
     }
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
